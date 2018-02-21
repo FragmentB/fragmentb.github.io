4bd02d4a5eede2937b68adfff2f1a4e6e59fe5e7
The Butler <sk066039@gmail.com>
2018-02-21 13:38:44 -0700

    Truing this again
    Want display to display again

--- Mists/mist.js
+++ Mists/mist.js
@@ -59,7 +59,8 @@ function create() {
 	defense = game.add.text((0+size),(screenHeight-(size*5))," ",style);
 	game.add.text((0+size),(screenHeight-(3*size)),"Cover:",style);
 	cover = game.add.text((0+size),(screenHeight-(size*2))," ",style);
-	info = game.add.text(((miniCols+2 * 0.6 * fontSize)+size),(screenHeight-(0*size)),defaultInfo,style);
+	info = game.add.text(((miniCols+2 * size)+size),(screenHeight-(size*2)),defaultInfo,style);
+	
 
 	//map
 	initMap();
@@ -945,4 +946,5 @@ function removeText(text) {
 
 function attack(actor, weapon){
 	info.content = "Attack Missed";
+	return 0;
 }
