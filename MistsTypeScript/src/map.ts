9c5fa64f48e5652cfa053fe2f6ab3d949ebab89a
Jessie Butler <sk066039@gmail.com>
2021-09-25 16:14:31 -0600

    Adding in a log statement temporarily.

--- MistsTypeScript/src/map.ts
+++ MistsTypeScript/src/map.ts
@@ -40,6 +40,7 @@ export class mapData{
                     mapObj.image = "";     
                 }
             }
+            console.log(mapObj);
             this.worldMap.push(mapObj);
         }
     }
