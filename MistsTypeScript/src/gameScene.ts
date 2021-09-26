08b07c41f8fbe0910e9fa338f4743d1f7a37f237
Jessie Butler <sk066039@gmail.com>
2021-09-25 22:09:53 -0600

    Adjusting scale to make tiles less yuge

--- MistsTypeScript/src/gameScene.ts
+++ MistsTypeScript/src/gameScene.ts
@@ -36,7 +36,7 @@ export class GameScene extends Phaser.Scene{
         var defaultInfo = "Welcome to the Mists";
         var screenHeight = Number(this.game.config.height);
         const bottomLayer = tileMap.createLayer(0,'tiles',0,0);
-        bottomLayer.setScale(size,size)
+        bottomLayer.setScale(3,3)
         //adding text displays for health, defense and cover
         this.add.text((0+size),(screenHeight-(12*size)),"Weapon:",style);
         var dispWeapon =this.add.text((0+size),(screenHeight-(size*11)),"",style);
