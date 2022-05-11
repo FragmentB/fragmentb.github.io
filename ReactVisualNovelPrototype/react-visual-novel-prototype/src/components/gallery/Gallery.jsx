42d9c5f15d87e9ae8fbe700bb380fe5e281c2a57
Jessie Butler <sk066039@gmail.com>
2022-05-11 05:15:59 -0600

    Start of the gallery function

--- ReactVisualNovelPrototype/react-visual-novel-prototype/src/components/gallery/Gallery.jsx
+++ ReactVisualNovelPrototype/react-visual-novel-prototype/src/components/gallery/Gallery.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import './Gallery.scss'
 import MainMenu from "components/main-menu/MainMenu";
+import { characterList } from '../../data/characterList'
 
 
 const Gallery = (props) => (
     <div className="galleryPage">
         <div className="galleryList">
             <h1>Gallery</h1>
+            <div className="gallerySpace">
+                {
+                    characterList.characters.map((character)=>
+                    <div className="galleryItem" key={character}>
+                        {character}
+                    </div>
+                    )
+                }
+            </div>
         </div>
         <MainMenu />
     </div>
---
+++ ReactVisualNovelPrototype/react-visual-novel-prototype/src/data/characterList.tsx
@@ -0,0 +1,25 @@
+export const characterList = {characters: [
+    "Misc",
+    "Lynn",
+    "Mika",
+    "Leena",
+    "Valerie",
+    "Pheonix",
+    "Kaori",
+    "Leoma",
+    "Layla",
+    "Connie",
+    "Cheryl",
+    "Cattos",
+    "Beverly",
+    "Arietta",
+    "Anya",
+    "Copper",
+    "Silver",
+    "Rishita",
+    "Saif",
+    "Kernesse",
+    "Annika"
+]}
+
+export default characterList;
